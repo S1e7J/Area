2c3982549c4e4ab2f14b3072ffc18ec18aa14be3
S1e7J <s.montoyar2@uniandes.edu.co>
2023-04-24 12:42:44 -0500

    Montecarlo

--- src/main.rs
+++ src/main.rs
@@ -29,21 +29,42 @@ fn Datos(data : &Result<Vec<Record>,Box<dyn Error>>)->Vec<Record> {
                 secondData = dato[i+1];
             }
             let m = (secondData.1-el.1)/(secondData.0-el.0);
-            let b = el.0-m*el.0;
+            let b = el.1-m*el.0;
             functions.push((m,b));
         }
     }
+    println!("{:?}",functions);
     return functions;
 }
 
 fn montecarlo(data : &Result<Vec<Record>,Box<dyn Error>>, functions : &Vec<Record>) {
     let total = 1_000_000;
-    let mut count = 0;
+    let mut count : i32 = 0;
     let mut rng = thread_rng();
-    for _ in 1..total {
-        let x = (2.0*rng.gen::<f64>());
-        let y = (2.0*rng.gen::<f64>());
-        
+    let mut cruces: i32 = 0;
+    if let Ok(dato) = data{
+        let len_Dato = dato.len()-1;
+        for _ in 1..total {
+            let x = (4.0*rng.gen::<f64>());
+            let y = (4.0*rng.gen::<f64>());
+            let m = (0.0-y)/(0.0-x);
+            let b = y-m*x;
+            for (i,el) in dato.iter().enumerate(){
+                let mut secondData = dato[0];
+                if i < len_Dato{
+                    secondData = dato[i+1];
+                }
+                let fun = functions[i];
+                let x1 = (b-fun.1)/(fun.0-x);
+                if (fun.0-m != 0.0 && x1 <= el.0 && x1 >= secondData.0){
+                    cruces += 1;
+                }
+            }
+            if (cruces).rem_euclid(2) != 0 {
+                count += 1;
+            }
+            println!("{:?}",count);
+        }
     }
 }
 
